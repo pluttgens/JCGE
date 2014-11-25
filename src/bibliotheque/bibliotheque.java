a7ecf32ab97fb897324b9ad080355d983f68c3b3
GenjitsuGame <pascal.luttgens@hotmail.fr>
2014-11-25 08:33:12 +0100

    game loop

--- src/bibliotheque/bibliotheque.java
+++ src/bibliotheque/bibliotheque.java
@@ -5,5 +5,35 @@ package bibliotheque;
  * @author scalpa
  */
 public class bibliotheque {
-    
-}
+
+    public void run() {
+        
+        boolean running = true;
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames + " TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+
+    }
