85b54dfda02e1e1a3f2d6b3a24fd7f2763a92be7
GenjitsuGame <pascal.luttgens@hotmail.fr>
2015-02-18 21:01:57 +0100

    Revert "trc avec collider"
    This reverts commit 4f5542aa869349ea767708572ea6ff6eaa6bec1e.

--- src/com/gamefactory/components/Collider.java
+++ src/com/gamefactory/components/Collider.java
@@ -2,35 +2,16 @@ package com.gamefactory.components;
 
 import com.gamefactory.displayable.Component;
 import com.gamefactory.displayable.ComponentManager;
-import com.gamefactory.displayable.GameObject;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Collider extends Component {
 
-    private ArrayList<GameObject> gameObjects = new ArrayList<>();
-    private ArrayList<GameObject> gameObjectsCollider = new ArrayList<>();
-    
     @Override
     public void init(ComponentManager owner) {
         super.init(owner);
-        gameObjects = owner.getScene().getGameObjects();
-        
-        Iterator<GameObject> it = gameObjects.iterator();
-        while (it.hasNext()) {
-            if(it.next().getComponentManager().checkForComponent("Collider")){
-             gameObjectsCollider.add(it.next());               
-            }
-        }
     }
 
     @Override
     public void update() {
-      Iterator<GameObject> it = gameObjectsCollider.iterator();
-            while (it.hasNext()) {
-                /*Position p = it.next().getComponentManager().;
-                if(){*/
-                    
-                }         
-            }
-        }
+    }
+
+}
