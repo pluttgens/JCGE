12f9530c02af6bd5c2e481f0b1034f318f81a76e
MrKe20 <kevngo26@hotmail.com>
2015-03-23 13:40:10 +0100

    Network

---
+++ src/com/gamefactory/network/Data.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gamefactory.network;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ngo
+ */
+public class Data implements Serializable {
+    
+    private static final long serialVersionUID = -5185336099759943530L;
+    
+}
