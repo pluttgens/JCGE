4e6db8279d71f24aa4b8d6ab9dc10474a61604e1
qsdfg <m1105.108.nero@gmail.com>
2015-03-24 16:26:35 +0100

    Collision
    Collision

--- src/com/gamefactory/components/Collider.java
+++ src/com/gamefactory/components/Collider.java
@@ -26,8 +26,11 @@ public class Collider extends Component {
         Iterator<GameObject> it = this.owner.getScene().iterateOverGO();
         while (it.hasNext()) {
             GameObject go = it.next();
+            String s1 = go.getId();
+            String s2 = this.owner.getOwner().getId();
+            if(s2.equals(s1)) break;
             if (go.getComponentManager().checkForComponent(Collider.class)) {
-                Collider col2 = (Collider) it.next().getComponentManager().getComponent(Collider.class);
+                Collider col2 = (Collider) go.getComponentManager().getComponent(Collider.class);
                 if (this.getHitbox().intersects(col2.getHitbox())) {
                     onEnterCollision(col2);
                 }
